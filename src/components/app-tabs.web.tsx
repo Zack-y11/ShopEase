5ebae5242a0c09e8228d28e31eac3f81cea07de7
Isaac <dev.zacknmr28@gmail.com>
2026-09-15 17:28:33 -0600

    refactor(routing): remove explore screen and update web tabs
    Delete the unused explore route and point the web tab bar to
    register instead.

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -24,8 +24,8 @@ export default function AppTabs() {
           <TabTrigger name="home" href="/" asChild>
             <TabButton>Home</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
-            <TabButton>Explore</TabButton>
+          <TabTrigger name="register" href="/register" asChild>
+            <TabButton>Register</TabButton>
           </TabTrigger>
         </CustomTabList>
       </TabList>
